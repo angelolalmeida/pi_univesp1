faafb178549c6d19fdd19e455c0e3d8076999e89
Angelo Almeida <angelolalmeida@yahoo.com.br>
2021-10-23 16:57:49 -0300

    fist commit

---
+++ projeto_integrador1/empreendedor/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# Create your models here.
+class Empreendedor(models.Model):
+    nome = models.CharField(max_length=80)
+    endereco = models.CharField(max_length=80)
+    bairro = models.CharField(max_length=80)
+    cidade = models.CharField(max_length=80)
+    uf = models.CharField(max_length=2)
+    whatsapp = models.CharField(max_length=11)
+    #id_usuario = models.ForeignKey()
